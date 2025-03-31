26e55be15cedbe41407549913e69c90524a5e58f
henrotaym <mathieu.henrotay@gmail.com>
2025-03-31 18:58:24 +0200

    🐛 Bad relative path for certs.

--- devops/generate_certs.sh
+++ devops/generate_certs.sh
@@ -3,5 +3,5 @@
 hostname=henrotaym
 mkcert -key-file traefik.key -cert-file traefik.crt \*.$hostname.ip-ddns.com
 mkcert -install
-mv traefik.crt ../certs/traefik.crt
-mv traefik.key ../certs/traefik.key
+mv traefik.crt ./certs/traefik.crt
+mv traefik.key ./certs/traefik.key
